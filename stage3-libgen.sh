d54e165313c0585adbc837db4ff2965bfdf243ec
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-09 00:41:23 +0200

    Add script for compile the stubs

--- stage3-libgen.sh
+++ stage3-libgen.sh
@@ -4,6 +4,14 @@ set -e
 ROOT=${PWD}
 PS4SDK=$ROOT/toolchain
 
-wget https://github.com/orbisdev/orbis-libs-gen/releases/latest/download/libs.tar.gz
+wget https://github.com/orbisdev/orbis-libs-gen/releases/latest/download/lib_s.tar.gz
 
-tar -zxvf libs.tar.gz
+tar -zxvf lib_s.tar.gz
+
+cd lib_s
+
+for i in *.S
+do
+    clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}.so $i
+    # echo "clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}.so $i"
+done
