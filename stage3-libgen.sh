5f4ac645b3641a7b422ca90ab1040486a071bbb9
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2020-05-10 18:21:01 +0200

    Update stage3-libgen.sh

--- stage3-libgen.sh
+++ stage3-libgen.sh
@@ -3,6 +3,7 @@ set -e
 
 ROOT=${PWD}
 PS4SDK=$ROOT/toolchain
+ARCHIVE=ar rcs
 ORBIS_BINUTILS=$ROOT/toolchain_temp/bin
 # We need orbis-ld
 PATH=$ORBIS_BINUTILS:$PATH
@@ -13,13 +14,15 @@ rm -rf lib_s
 tar -zxvf lib_s.tar.gz
 cd lib_s
 
-for d in */ ; do
+for d in `ls -d *`
+do
     cd $d
     for i in *.S ; do
         clang --target=x86_64-scei-ps4 $i -c
-        ar -rcs ${i%.S}_stub.a ${i%.S}.o
     done
+    ls *.o > ${d}_list.tmp
+    xargs ${archive} ${d}_stub.a < ${d}_list.tmp
     cd ..
 done
 
-find . -name '*.a' -exec cp --parents \{\} $PS4SDK/usr/lib \;
+find . -name '*.a' -exec cp \{\} $PS4SDK/usr/lib \;
