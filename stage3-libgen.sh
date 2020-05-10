e3ecf9ba348e236ca9c89b729335ffe4984aa371
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-10 12:31:37 +0200

    Generate .a instead of .so for the stubs

--- stage3-libgen.sh
+++ stage3-libgen.sh
@@ -15,8 +15,8 @@ cd lib_s
 
 for i in *.S
 do
-    clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}.so $i
-    # echo "clang --target=x86_64-scei-ps4 -shared -nostdlib -o  ${i%.S}.so $i"
+    clang --target=x86_64-scei-ps4 $i -c
+    ar -rcs ${i%.S}.a ${i%.S}.o
 done
 
-cp *.so $PS4SDK/usr/lib
+cp *.a $PS4SDK/usr/lib
