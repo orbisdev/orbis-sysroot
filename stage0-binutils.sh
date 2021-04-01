692acc53c10f299574bbb17c1623abf64cfdabe1
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-04-01 19:46:03 +0200

    Update binutils script

--- stage0-binutils.sh
+++ stage0-binutils.sh
@@ -4,8 +4,17 @@ set -e
 ROOT=${PWD}
 PS4SDK=$ROOT/toolchain_temp
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
-git clone --depth 1 https://github.com/bminor/binutils-gdb
-cd binutils-gdb
+
+## Download the source code.
+REPO_URL="https://github.com/bminor/binutils-gdb"
+REPO_FOLDER="binutils-gdb"
+BRANCH_NAME="binutils-2_36_1"
+if test ! -d "$REPO_FOLDER"; then
+	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+else
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} || exit 1
+fi
+
 ./configure --prefix=$PS4SDK --target="x86_64-pc-freebsd9" \
 	--disable-nls \
 	--disable-dependency-tracking \
@@ -14,8 +23,12 @@ cd binutils-gdb
 	--enable-lto \
 	--enable-plugins \
 	--enable-poison-system-directories
-make -j $PROC_NR
-make install
+
+## Compile and install.
+make --quiet -j $PROC_NR clean   || { exit 1; }
+make --quiet -j $PROC_NR || { exit 1; }
+make --quiet -j $PROC_NR install-strip || { exit 1; }
+make --quiet -j $PROC_NR clean   || { exit 1; }
 
 cd $PS4SDK/bin
 ln -s x86_64-pc-freebsd9-ld orbis-ld
