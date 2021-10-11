ef64f09b7043b3f2262aa74e6741d5c40389a6a5
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2021-10-11 23:28:02 +0200

    delete ls

--- build.sh
+++ build.sh
@@ -6,7 +6,5 @@ sh stage0-binutils.sh
 sh stage1-headers.sh
 sh stage2-libgen.sh
 sh stage3-libcxx.sh
-ls -l usr/include/c++/v1
-ls -l usr/lib
 # sh stage4-ps4sdk-headers.sh
 # tar -zcvf toolchain.tar.gz toolchain
