780d42eed211a6ec3e8c504430c3d9f02c1b2d46
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2021-10-11 22:33:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ sh prestage0-llvm.sh
 sh stage0-binutils.sh
 sh stage1-headers.sh
 sh stage2-libgen.sh
-# sh stage3-libcxx.sh
+sh stage3-libcxx.sh
 # sh stage4-ps4sdk-headers.sh
 # tar -zcvf toolchain.tar.gz toolchain
